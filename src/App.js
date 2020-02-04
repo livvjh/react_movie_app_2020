d4d76b692e0f2efb01a9dd17099b846c86c26921
jhle-e <jhlee7129@gmail.com>
2020-02-04 17:58:59 +0900

    리액트 배열 내장 함수 map을 통한 component 반복 예제 코드 추가

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Food({favorite}) {
-  console.log({favorite});
-  return (<h2>I like {favorite}</h2>)
+
+function Clubs({ name }) {
+  return <h1>I like {name}</h1>;
 }
+
+const clubs = [
+  {
+    name : "chelsea",
+    image : "/Users/leejihwan/Downloads/images.png"
+  },
+  {
+    name : "liverpool",
+    image : "/Users/leejihwan/Downloads/61H5-6IthRL._AC_SY606_.jpg"
+  },
+  {
+    name : "arsenal",
+    image : "/Users/leejihwan/Downloads/Arsenal-logo.png"
+  }
+]
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {clubs.map(dish => (
+          <Clubs name = {dish.name} />
+        ))} 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-          <Food favorite="kimchi" />
-          <Food favorite="ramen" />
-          <Food favorite="oyster" />
-          <Food favorite="bulgogi" />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
