353507841b687da24e3ed98252bbf9dd8d268c03
jhle-e <jhlee7129@gmail.com>
2020-02-04 18:07:08 +0900

    item명 변경 및 img 경로 추가

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function Clubs({ name }) {
-  return <h1>I like {name}</h1>;
+function Clubs({ name, picture }) {
+  return (
+    <div>
+      <h1>I like { name }</h1>
+      <img alt="emblem" src={ picture }/>
+    </div>
+  );
 }
 
 const clubs = [
   {
     name : "chelsea",
-    image : "/Users/leejihwan/Downloads/images.png"
+    image : "https://upload.wikimedia.org/wikipedia/ko/thumb/a/ae/Chelsea_FC_Logo.svg/1024px-Chelsea_FC_Logo.svg.png"
   },
   {
     name : "liverpool",
-    image : "/Users/leejihwan/Downloads/61H5-6IthRL._AC_SY606_.jpg"
+    image : "https://images-na.ssl-images-amazon.com/images/I/61H5-6IthRL._AC_SY606_.jpg"
   },
   {
     name : "arsenal",
-    image : "/Users/leejihwan/Downloads/Arsenal-logo.png"
+    image : "https://worldsportlogos.com/wp-content/uploads/2018/02/Arsenal-Emblem.png"
   }
 ]
 
@@ -28,8 +33,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {clubs.map(dish => (
-          <Clubs name = {dish.name} />
+        {clubs.map(item => (
+          <Clubs name = {item.name} picture = {item.image}/>
         ))} 
         <p>
           Edit <code>src/App.js</code> and save to reload.
