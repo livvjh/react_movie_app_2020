dc440c3bc56b31e29b4354dc08ec8ce7a324fa06
jhle-e <jhlee7129@gmail.com>
2020-02-06 01:58:20 +0900

    화면 css 구성 추가 및 summary slice 처리

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
         <div className="movie">
             <img src={poster} alt={title}></img>
@@ -11,7 +11,12 @@ function Movie({ year, title, summary, poster }) {
                     {title}
                 </h3>
                 <h5 className="movie_year">{year}</h5>
-                <p className="movie_summary">{summary}</p>
+                <ul className="movie_genres">
+                    {genres.map((genre, index)=> {
+                       return <li key={index} className="genres_list">{genre}</li>
+                    })}    
+                </ul>
+                <p className="movie_summary">{summary.slice(0, 140)}...</p>
             </div>
         </div>
     );
@@ -22,7 +27,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
