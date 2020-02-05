cf3e2b5dbfcb2d24f4dd9a4de5c92e349ff0d7e3
jhle-e <jhlee7129@gmail.com>
2020-02-05 18:49:51 +0900

    혼자 date 시간 적용해보기

--- src/App.js
+++ src/App.js
@@ -4,21 +4,35 @@ import './App.css';
 
 class App extends React.Component {
     state = {
-      count : 0
+      count : 0,
+      date : new Date()
     }  
 
     add = () => {
-      this.setState({count : this.state.count + 1})
-      // this.setState(current => ({ count : current.count + 1 }))
+      // this.setState({count : this.state.count + 1})
+      this.setState(current => ({ count : current.count + 1 }))
     }
     remove = () => {
       this.setState(current => ({ count : current.count - 1 }))
     }
 
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(), 1000
+      )
+    }
+
+    tick() {
+      this.setState({
+        date : new Date()
+      })
+    }
+
     render() {
       return (
         <div>
-          <h1>the number is : {this.state.count}</h1>
+          <h1>It is { this.state.date.toLocaleTimeString() } </h1>
+          <h2>the number is : {this.state.count}</h2>
           <button onClick={this.add}>Add</button>
           <button onClick={this.remove}>Remove</button>
         </div>
