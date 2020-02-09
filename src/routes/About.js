129870794fb569c73e8b3a4abcd6d2ecdd6f3b3b
jhle-e <jhlee7129@gmail.com>
2020-02-10 01:12:23 +0900

    route 및 gh-pages 적용

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+    return (
+        <div className="about_container">
+            <span>About page</span>
+            <span>i like Movies and animal</span>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push("/");
+        }
+    }
+    render() {
+        const { location } = this.props;
+            if(!location.state) {
+                return null
+            } else {
+                return <span>{location.state.title}</span>
+            }
+    }
+}
+
+export default Detail;
