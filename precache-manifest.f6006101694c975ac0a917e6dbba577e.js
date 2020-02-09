aeb24bf43927226c2b3025cc94e60cb0824b86f6
jhle-e <jhlee7129@gmail.com>
2020-02-09 18:24:04 +0900

    Updates

---
+++ precache-manifest.f6006101694c975ac0a917e6dbba577e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c52544da89ec29c1641b1ea5abe3fa4",
+    "url": "/react_movie_app_2020/index.html"
+  },
+  {
+    "revision": "a2fe2cd6b15c5ddf65c4",
+    "url": "/react_movie_app_2020/static/css/main.09c6c55d.chunk.css"
+  },
+  {
+    "revision": "5fcc70e338eb4313f507",
+    "url": "/react_movie_app_2020/static/js/2.4233df8c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_movie_app_2020/static/js/2.4233df8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fe2cd6b15c5ddf65c4",
+    "url": "/react_movie_app_2020/static/js/main.27653516.chunk.js"
+  },
+  {
+    "revision": "16a64b064fe13a631843",
+    "url": "/react_movie_app_2020/static/js/runtime-main.69d2885d.js"
+  }
+]);
