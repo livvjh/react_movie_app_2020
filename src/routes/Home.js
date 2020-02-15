c95b34ad78ee60ae873828bd7508db1af2d7d7f0
jhle-e <jhlee7129@gmail.com>
2020-02-15 17:10:14 +0900

    2020_0215 - 리액트 컴포넌트 실행 순서 및 state 상태값 기능 주석 설명 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -19,9 +19,20 @@ class Home extends React.Component {
         this.setState({ movies, isLoading: false });
     };
     componentDidMount() {
+        //render() 이후 실행
+        console.log(2);
         this.getMovies();
     }
     render() {
+        // 첫번째 실행 (렌더링)
+        // 아래의 this.state 값은 초기 state값과 동일
+        // 이후 componentDidMount가 실행된 후 getMovies를 통해 axios 결과값을 가져온다
+        // 이후 가져온 rs를 this.setState 함수를 통해 값을 초기화 
+        // render() 안에 리턴되는 데이터는 this.state를 통해 값을 가져와 브라우저에 렌더링처리
+
+        console.log(1);
+        console.log(this.state.movies);
+        console.log(this.state.isLoading);
         const { isLoading, movies } = this.state;
         return (
             <section className="container">
